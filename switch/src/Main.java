662634a47715e9d22e07204373d458078aba7862
syllexxbtw <syllexxbtw@gmail.com>
2024-11-13 07:13:46 +0300

    Add files via upload

---
+++ switch/src/Main.java
@@ -0,0 +1,38 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите номер дня недели (1-7): ");
+        int N = in.nextInt();
+
+        switch (N) {
+            case 1:
+                System.out.println("Понедельник");
+                break;
+            case 2:
+                System.out.println("Вторник");
+                break;
+            case 3:
+                System.out.println("Среда");
+                break;
+            case 4:
+                System.out.println("Четверг");
+                break;
+            case 5:
+                System.out.println("Пятница");
+                break;
+            case 6:
+                System.out.println("Суббота");
+                break;
+            case 7:
+                System.out.println("Воскресенье");
+                break;
+            default:
+                System.out.println("Ошибка");
+        }
+    }
+}
